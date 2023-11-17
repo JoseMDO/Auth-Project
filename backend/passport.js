ce344cb55ad389a6c4d4bb809946154fbc670886
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-11-17 00:37:00 -0600

    completed github auth

--- backend/passport.js
+++ backend/passport.js
@@ -1,9 +1,15 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GitHubStrategy = require('passport-github2')
+const FacebookStrategy = require('passport-facebook')
 const passport = require('passport')
 
 
 const GOOGLE_CLIENT_ID = "20848583384-5u1fqp9tpcvojhu3c48kjf0tnfphj2t3.apps.googleusercontent.com"
 const GOOGLE_CLIENT_SECRET = "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"
+
+const GITHUB_CLIENT_ID = "e604a0458b19b95cdfca"
+const GITHUB_CLIENT_SECRET = "c32d3b7fb5dd5b0a1e671000b1e75f73fee187f6"
+
 
 
 passport.use(new GoogleStrategy({
@@ -19,6 +25,28 @@ passport.use(new GoogleStrategy({
     done(null, profile)
   }
 ));
+
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    done(null, profile)
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    return cb(err, user);
+  }
+));
+
 
 passport.serializeUser((user, done) => {
     console.log("SERIALIZED")
